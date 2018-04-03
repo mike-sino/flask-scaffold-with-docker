d2c524ab30a4f69dae81a89dc847aec0c0994dba
Mike <chutian.8986@gmail.com>
2018-04-03 18:34:40 +0900

    refresh scaffold

--- flask-scaffold/tests.bash
+++ flask-scaffold/tests.bash
@@ -1,19 +1,4 @@
 #!/bin/bash
 
-
-
-
 #TESTS
-#Tests for authors
-protractor  app/templates/authors/conf.js  &&
-python app/authors/test_authors.py
-#End Tests for authors
-#Tests for comments
-protractor  app/templates/comments/conf.js  &&
-python app/comments/test_comments.py
-#End Tests for comments
-#Tests for posts
-protractor  app/templates/posts/conf.js  &&
-python app/posts/test_posts.py
-#End Tests for posts
 
